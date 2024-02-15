fca8b4fb8e6d220a8b2bfd169301f0da9a3223e2
LockBall <38836349+LockBall@users.noreply.github.com>
2024-02-15 14:11:01 -0500

    renames files with specific extension, no longer renames itself

--- total_rename_all.py
+++ total_rename_all.py
@@ -3,26 +3,29 @@
 import os
 
 def rename_files():
-
-    new_filename = "new_filename"
-
-    current_directory = os.getcwd()
-    print("Renames files in ", current_directory)
-
     count = 0
+    new_filename = "new_filename"
+    filetype = ".png"
+    current_directory = os.getcwd()
+    print("Renaming files in ", current_directory)
 
-    for filename in os.listdir(current_directory):
+    for file in os.listdir(current_directory):
+        split_tup = os.path.splitext(str(file)) # 0 - name, 1 = ext
+        # print("split_tup", split_tup)
+
+        if split_tup[1] == filetype :
+
+            print("filename: ", file)
+
+            new_file = new_filename + str(count) + filetype
+            print("new_file: ", new_file)
+
+            os.rename(file, new_file)
         
-        source = filename  # Construct old file name
-        # print("source: ", source)
-
-        destination = new_filename + str(count) + ".png"
-        # print("destination: ", destination)
-
-        os.rename(source, destination)
-     
-        count = count + 1
-    
+            count = count + 1
+        # endif split_tup
+    # endfor file
+            
     print(count, " ", " files counted")
 
 if __name__ == "__main__":
